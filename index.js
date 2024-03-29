c22075a0ea92db90683de137306e200ebc16f6cc
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-03-29 14:20:38 +0300

    Modify

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+// find matchingDrivers
+function findMatching(drivers, query) {
+return drivers.filter(driver => 
+    driver.toLowerCase() === query.toLowerCase()
+    );
+}
+// finding fuzzyMatch
+ function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(letters.toLowerCase())  
+      );
+ }
+//  finding matchingNames
+function matchName(drivers, name) {
+    return drivers.filter(driver => 
+     driver.name.toLowerCase() === name.toLowerCase() 
+     );
+}
